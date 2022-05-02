18b944173ce5de7a3632a0f4176c3a877fff15f6
Irina Rubinshtein <rubinshteiniv@gmail.com>
2022-05-02 20:55:55 +0300

    @ParameterizedTest

---
+++ src/test/java/qa/quru/WebTest.java
@@ -0,0 +1,47 @@
+package qa.quru;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WebTest {
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("https://simplewine.ru/");
+        element(byClassName("js-age-confirm")).shouldBe(visible).click();
+        element(byClassName("ui-location__button")).click();
+    }
+
+    @CsvSource(value = {
+            "ВИНО, Максимально полное собрание сочинений на тему вина",
+            "КОНЬЯК, Коллекция коньяков от первых Домов Франции"
+    })
+    @ParameterizedTest(name = "Проверка перехода в нужный раздел каталога по названию {0}")
+    void simpleMenuTest(String testData, String expectedResult) {
+        List<SelenideElement> menuList = elements(byClassName("navigation__link-text"));
+        menuList.stream().filter(it -> it.getText().equals(testData)).findFirst().get().click();
+        element(byClassName("image-title"))
+                .shouldBe(visible)
+                .shouldHave(text(expectedResult));
+    }
+
+    @AfterEach
+    void clearBrowser() {
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+        Selenide.closeWebDriver();
+
+    }
+}
